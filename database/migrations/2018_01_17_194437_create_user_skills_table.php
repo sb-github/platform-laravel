02a7735cbe32c41164254a84a98f042e03c85962
kmHarisUa <jail21@mail.ru>
2018-01-19 15:47:45 +0200

    Basic

---
+++ database/migrations/2018_01_17_194437_create_user_skills_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('skill_id')->unsigned();
+            $table->timestamps();
+        });
+		
+		Schema::table('user_skills', function (Blueprint $table) {
+			$table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('skill_id')->references('id')->on('skills');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_skills');
+    }
+}
