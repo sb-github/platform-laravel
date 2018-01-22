5ab57de39b9ba0644db6a83c7c50ac006ddcf2c9
Arthur Borikun <arturborikun2@gmail.com>
2018-01-22 21:41:26 +0200

    created merge controller

---
+++ app/Http/Controllers/MergeController.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\GraphSkill;
+use App\GraphSkillVacancy;
+use App\Vacancy;
+use GuzzleHttp;
+use Illuminate\Http\Request;
+
+
+class MergeController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function skills($requestedSkill)
+    {
+        if(strcmp($requestedSkill,"all") == 0)
+        {
+            $result = Skill::select('name')
+                ->get()
+                ->toJson();
+        }
+        else
+        {
+            $idRequestedSkill = Skill::select('id')
+                ->where('name', $requestedSkill)
+                ->first()
+                ->toArray()['id'];
+
+            $result = GraphSkill::select('parent_skill', 'related_skill')
+                ->where('parent_skill', $idRequestedSkill)
+                ->orWhere('related_skill', $idRequestedSkill)
+                ->get()
+                ->toArray();
+
+            $idRelatedSkills = [];
+            foreach ($result as $item)
+            {
+                $idRelatedSkills[] = $item['parent_skill'];
+                $idRelatedSkills[] = $item['related_skill'];
+            }
+
+            $result = Skill::select('name')
+                ->whereIn('id', array_unique($idRelatedSkills))
+                ->get()
+                ->toJson();
+        }
+
+        $skills = [];
+
+        foreach (json_decode($result) as $item)
+            $skills[] = $item->name;
+
+        return $skills;
+    }
+    public function merge(Request $request, $requestedSkill)
+    {
+        $skills = $this->skills($requestedSkill);
+        //$crawler = $request->input('crawler_id');
+        $crawler = 'test';
+
+        $result = $this->callAPI($crawler, $requestedSkill, $skills);
+
+        foreach ($result as $skill)
+            $this->graphMerger($skill);
+    }
+    public function callAPI($crawler,$requestedSkill, $skills)
+    {
+        $client = new GuzzleHttp\Client();
+
+        $res = $client->post('localhost:3001/api/graph/' . $requestedSkill, [
+            GuzzleHttp\RequestOptions::JSON => [
+                'crawler_id' => $crawler,
+                'skills' => $skills
+            ]
+        ]);
+
+        return json_decode($res->getBody());
+    }
+    public function graphMerger($skill)
+    {
+        $parentSkill = Skill::select('id')
+            ->where('name', $skill->skill)
+            ->first()
+            ->toArray()['id'];
+
+        foreach ($skill->connects as $key => $relatedSkill)
+        {
+            $relation = $this->relationMerger($parentSkill, $relatedSkill, $skill->date);
+
+            $vacancies = $this->linksMerger($relatedSkill->links);
+
+            $this->vacanciesMerger($vacancies, $relation->id, $relation->last_date);
+        }
+
+    }
+    public function relationMerger($parentSkill, $relatedSkill, $lastDate)
+    {
+        $relatedSkill->id = Skill::select('id')
+            ->where('name', $relatedSkill->subskill)
+            ->first()
+            ->toArray()['id'];
+
+        $graphRelation = GraphSkill::select('id', 'last_date')
+            ->where([
+                ['parent_skill', $parentSkill],
+                ['related_skill', $relatedSkill->id]
+            ])
+            ->orWhere([
+                ['parent_skill', $relatedSkill->id],
+                ['related_skill', $parentSkill]
+            ])
+            ->first();
+
+        if(!isset($graphRelation))
+        {
+            GraphSkill::insert([
+                'parent_skill' => $parentSkill,
+                'related_skill' => $relatedSkill->id,
+                'weight' => $relatedSkill->weight,
+                'last_date' => date('Y-m-d H:i:s', $lastDate)
+            ]);
+
+            $graphRelation = GraphSkill::select('id', 'last_date')
+                ->where([
+                    ['parent_skill', $parentSkill],
+                    ['related_skill', $relatedSkill->id]
+                ])
+                ->orWhere([
+                    ['parent_skill', $relatedSkill->id],
+                    ['related_skill', $parentSkill]
+                ])
+                ->first();
+        }
+        elseif(date('Y-m-d H:i:s', $lastDate) > $graphRelation->last_date)
+        {
+            $graphRelation->last_date = date('Y-m-d H:i:s', $lastDate);
+            $graphRelation->weight = $relatedSkill->weight;
+            $graphRelation->save();
+        }
+
+        $graphRelation = json_decode($graphRelation->toJson());
+
+        return $graphRelation;
+    }
+    public function linksMerger($links)
+    {
+        $links = array_unique($links);
+
+        $result = Vacancy::select('id','link')
+            ->whereIn('link', $links)
+            ->get()->toJson();
+
+        $oldLinks = [];
+        $linksId = [];
+
+        foreach (json_decode($result) as $item)
+        {
+            $oldLinks[] = $item->link;
+            $linksId[] = $item->id;
+        }
+
+        foreach (array_diff($links, $oldLinks) as $item)
+        {
+            Vacancy::insert([
+                'link' => $item
+            ]);
+
+            $linksId[] = Vacancy::select('id')
+                ->where('link', $item)
+                ->first()
+                ->toArray()['id'];
+        }
+
+        return $linksId;
+    }
+    public function vacanciesMerger($vacancies, $graphSkill, $lastDate)
+    {
+        $result = GraphSkillVacancy::select('vacancy_id')
+            ->where('graph_skill_id', $graphSkill)
+            ->get()
+            ->toJson();
+
+        $oldVacancies = [];
+
+        foreach (json_decode($result) as $item)
+            $oldVacancies[] = $item->vacancy_id;
+
+        foreach (array_diff($vacancies, $oldVacancies) as $item)
+            GraphSkillVacancy::insert([
+                'graph_skill_id' => $graphSkill,
+                'vacancy_id' => $item,
+                'last_date' => date('Y-m-d H:i:s', strtotime($lastDate))
+            ]);
+
+        GraphSkillVacancy::whereIn('vacancy_id', $oldVacancies)
+            ->where([
+                ['graph_skill_id', $graphSkill],
+                ['last_date', '<', date('Y-m-d H:i:s', strtotime($lastDate))]
+            ])
+            ->update([
+                'last_date' => date('Y-m-d H:i:s', strtotime($lastDate))
+            ]);
+    }
+
+    public function test()
+    {
+        echo 'Hello';
+    }
+}
